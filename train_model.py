6997a3e8a34ea32a496386d8b113251a53e9c517
GOPATHI-RISHITHA <rishithagopathi@gmail.com>
2026-03-14 10:56:53 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,42 +1,63 @@
-import tensorflow as tf
-from tensorflow.keras import layers, models
+# import tensorflow as tf
+# from tensorflow.keras import layers, models
 
-# Load dataset
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# # Load dataset
+# (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# Normalize
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+# # Normalize
+# x_train = x_train / 255.0
+# x_test = x_test / 255.0
 
-# Reshape
-x_train = x_train.reshape(-1,28,28,1)
-x_test = x_test.reshape(-1,28,28,1)
+# # Reshape
+# x_train = x_train.reshape(-1,28,28,1)
+# x_test = x_test.reshape(-1,28,28,1)
 
-# CNN model
-model = models.Sequential([
-    layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)),
-    layers.MaxPooling2D((2,2)),
+# # CNN model
+# model = models.Sequential([
+#     layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)),
+#     layers.MaxPooling2D((2,2)),
 
-    layers.Conv2D(64,(3,3),activation='relu'),
-    layers.MaxPooling2D((2,2)),
+#     layers.Conv2D(64,(3,3),activation='relu'),
+#     layers.MaxPooling2D((2,2)),
 
-    layers.Flatten(),
+#     layers.Flatten(),
 
-    layers.Dense(128,activation='relu'),
-    layers.Dense(10,activation='softmax')
-])
+#     layers.Dense(128,activation='relu'),
+#     layers.Dense(10,activation='softmax')
+# ])
 
-# Compile
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
-)
+# # Compile
+# model.compile(
+#     optimizer='adam',
+#     loss='sparse_categorical_crossentropy',
+#     metrics=['accuracy']
+# )
 
-# Train
-model.fit(x_train,y_train,epochs=5)
+# # Train
+# model.fit(x_train,y_train,epochs=5)
 
-# Save model
-model.save("digit_cnn_model.h5")
+# # Save model
+# model.save("digit_cnn_model.h5")
 
-print("Model trained and saved")
+# print("Model trained and saved")
+
+
+import joblib
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+digits = load_digits()
+
+X = digits.data
+y = digits.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = SVC()
+
+model.fit(X_train, y_train)
+
+joblib.dump(model, "digit_model.pkl")
+
+print("Model saved")
