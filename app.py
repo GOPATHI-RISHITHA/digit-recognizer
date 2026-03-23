92cc82da139ca7e7a9b40cbe46a39846ba90e26b
GOPATHI-RISHITHA <rishithagopathi@gmail.com>
2026-03-23 14:05:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,63 @@
 import streamlit as st
+import torch
+import torch.nn as nn
 import numpy as np
-import joblib
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
-# Load trained model
-model = joblib.load("digit_model.pkl")
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1,32,3,1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32,64,3,1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(64*5*5,128),
+            nn.ReLU(),
+            nn.Linear(128,10)
+        )
 
-st.title("Handwritten Digit Recognizer")
+    def forward(self,x):
+        x = self.conv(x)
+        x = x.view(-1,64*5*5)
+        return self.fc(x)
 
-st.write("Draw a digit (0–9)")
+model = CNN()
+model.load_state_dict(torch.load("digit_cnn.pth", map_location="cpu"))
+model.eval()
+
+st.title("AI Digit Recognizer")
 
 canvas_result = st_canvas(
     fill_color="black",
-    stroke_width=15,
+    stroke_width=25,
     stroke_color="white",
     background_color="black",
     height=280,
     width=280,
     drawing_mode="freedraw",
-    key="canvas",
 )
 
 if canvas_result.image_data is not None:
-
     img = canvas_result.image_data
 
-    # Convert to grayscale
     img = Image.fromarray(img.astype("uint8")).convert("L")
+    img = img.resize((28,28))
 
-    # Resize to dataset size
-    img = img.resize((8, 8))
+    img = np.array(img)
+    img = 255 - img
+    img = img / 255.0
 
-    # Convert to numpy
-    img_array = np.array(img)
+    img = img.reshape(1,1,28,28)
+    img = torch.tensor(img, dtype=torch.float32)
 
-    # Invert colors
-    img_array = 255 - img_array
+    with torch.no_grad():
+        output = model(img)
+        pred = torch.argmax(output,1).item()
 
-    # Normalize
-    img_array = img_array / 16.0
-
-    # Flatten
-    img_array = img_array.flatten().reshape(1, -1)
-
-    prediction = model.predict(img_array)
-
-    st.subheader(f"Predicted Digit: {prediction[0]}")
+    st.subheader(f"Prediction: {pred}")
